54f8f855e2888f06c943261a40df33b88e53a26f
PabloSan1997 <jos_pab_997@hotmali.com>
2023-06-28 17:14:23 -0600

    Se quito env

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/PabloSan1997/ApiGoPractica/handler"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Error con la variables de entorno")
-	}
-	PORT := os.Getenv("PORT")
-	if PORT == "" {
-		PORT = ":3001"
-	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handler.IndexRoute).Methods("GET")
 	router.HandleFunc("/api/v1/imagenes", handler.LeerImagenes).Methods("GET")
@@ -27,5 +17,5 @@ func main() {
 	router.HandleFunc("/api/v1/imagenes", handler.AgregarImagen).Methods("POST")
 	router.HandleFunc("/api/v1/imagenes/{id}", handler.EliminarImagen).Methods("DELETE")
 	router.HandleFunc("/api/v1/imagenes", handler.EditarImagen).Methods("PUT")
-	log.Fatal(http.ListenAndServe(PORT, router))
+	log.Fatal(http.ListenAndServe(":3000", router))
 }

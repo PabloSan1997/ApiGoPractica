b6a1bebb8fadfccdacf5e7d952774176b2815449
PabloSan1997 <jos_pab_997@hotmali.com>
2023-06-28 17:46:34 -0600

    final

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PabloSan1997/ApiGoPractica/handler"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handler.IndexRoute).Methods("GET")
 	router.HandleFunc("/api/v1/imagenes", handler.LeerImagenes).Methods("GET")
@@ -17,5 +17,5 @@ func main() {
 	router.HandleFunc("/api/v1/imagenes", handler.AgregarImagen).Methods("POST")
 	router.HandleFunc("/api/v1/imagenes/{id}", handler.EliminarImagen).Methods("DELETE")
 	router.HandleFunc("/api/v1/imagenes", handler.EditarImagen).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Fatal(http.ListenAndServe(os.Getenv("PORT"), router))
 }
